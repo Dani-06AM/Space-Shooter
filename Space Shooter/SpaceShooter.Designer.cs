267da132b8748a0b44f9c080da0b1cbe92df879e
Dani <dherreror@gmail.com>
2025-06-15 21:47:55 +0200

    Agregar archivos de proyecto.

---
+++ Space Shooter/SpaceShooter.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Space_Shooter
+{
+    partial class SpaceShooter
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpaceShooter));
+            this.BgSpeed = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // BgSpeed
+            // 
+            this.BgSpeed.Enabled = true;
+            this.BgSpeed.Interval = 10;
+            this.BgSpeed.Tick += new System.EventHandler(this.BgSpeed_Tick);
+            // 
+            // SpaceShooter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 500);
+            this.Name = "SpaceShooter";
+            this.Text = "Space Shooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer BgSpeed;
+    }
+}
+
---
+++ Space Shooter/SpaceShooter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Shooter
+{
+    public partial class SpaceShooter : Form
+    {
+        PictureBox[] stars;
+        int BackgroundSpeed;
+        Random rnd;
+
+        public SpaceShooter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackgroundSpeed = 4;
+            stars = new PictureBox[20];
+            rnd = new Random();
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(rnd.Next(20, 580), rnd.Next(-10, 400));
+
+                if (i % 2 == 1)
+                {
+                    stars[i].Size = new Size(2, 2);
+                    stars[i].BackColor = Color.White;
+                }
+                else
+                {
+                    stars[i].Size = new Size(3, 3);
+                    stars[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(stars[i]);
+
+            }
+
+        }
+
+        private void BgSpeed_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < stars.Length/2; i++)
+            {
+                stars[i].Top += BackgroundSpeed;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
+
+            for (int i = stars.Length/2; i < stars.Length;  i++)
+            {
+                stars[i].Top += BackgroundSpeed - 2;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
+        }
+    }
+}
